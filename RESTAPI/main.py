3785afba470c7652cdfb247997cd2904a8e695e2
Vishwa <vishwanathavin@gmail.com>
2017-12-06 18:14:03 +0530

    intermediate

--- RESTAPI/main.py
+++ RESTAPI/main.py
@@ -3,18 +3,18 @@ from flask_pymongo import PyMongo
 
 
 app=Flask(__name__)
-# app.config['MONGO_DBNAME'] = 'ssmatri'
-# app.config['MONGO_URI'] = 'mongodb://localhost:27017/ssmatri'
-# mongo = PyMongo(app)
+app.config['MONGO_DBNAME'] = 'ssmatri'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/ssmatri'
+mongo = PyMongo(app)
 
 @app.route('/', methods=['POST'])
 def root():
 
     name = request.form.get('name')
     # ID = request.form.get('ID')
-    stars_ = request.form.get('stars')
-    gothram_ = request.form.get('gothram')
-    subsect_ = request.form.get('subsect')
+    STAR_noPadham = request.form.get('stars')
+    GOTHRAM = request.form.get('gothram')
+    SUBSECT = request.form.get('subsect')
     # qualification_ = request.form.get('qualification')
     age_From = request.form.get('ageFrom')
     age_TO = request.form.get('ageTo')
@@ -22,12 +22,20 @@ def root():
     height_To = request.form.get('heightTo')
 
     # Get all the input
+    filterList=["STAR_noPadham","GOTHRAM","SUBSECT","age_From","age_TO","height_From","height_To"]
 
-    # print and see respose
+    queryList=[]
+    for filter in filterList:
+        if filter is not None:
+            if filter in [STAR_noPadham,GOTHRAM,SUBSECT]:
+                queryList.append({filter:filter})
+
+
 
     # do a find filter in mongodb
-
-
+    output = mongo.db.personInfo.find({"$and":[{"STAR_noPadham":STAR_noPadham},{"SUBSECT":subsect_}]})
+    for val in output:
+        print(val)
     response = {'message': "Welcome to matri!."+name}
 
     return jsonify(response)
